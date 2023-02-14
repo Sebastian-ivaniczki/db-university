70d23ce8c32695a52601771f5f4358c8ba2eeefc
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-14 14:43:41 +0100

    ex 6

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -18,7 +18,7 @@ SELECT * FROM `courses` WHERE `period` LIKE 'I semestre' AND `year` LIKE '1';
 SELECT * FROM `exams` WHERE `date` = DATE('2020-06-20') AND `hour` >= TIME('14:00:00');
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
-
+SELECT * FROM `degrees` WHERE `level` LIKE '%magistrale%';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
 
