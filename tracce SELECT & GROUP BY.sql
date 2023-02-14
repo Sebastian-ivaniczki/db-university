c5b88b7e8b1b256dcf631f214ae2f00bd1fe58dc
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-14 17:13:51 +0100

    fix ex 3

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -9,7 +9,7 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
 
-SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992;
+SELECT * FROM students WHERE YEAR(date_of_birth) < YEAR(DATE_SUB(CURRENT_DATE(), INTERVAL 30 YEAR));
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
 SELECT * FROM `courses` WHERE `period` LIKE 'I semestre' AND `year` LIKE '1';
