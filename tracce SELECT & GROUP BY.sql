a10263d4142df5c3038dd083fb7910567c3e8132
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-14 14:35:28 +0100

    ex 4 done

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -12,7 +12,7 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992;
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-
+SELECT * FROM `courses` WHERE `period` LIKE 'I semestre' AND `year` LIKE '1';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
 
