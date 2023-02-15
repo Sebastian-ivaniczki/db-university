d86da65a4c8a4f74ee1150f60a09cadd5dfe69db
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-15 15:03:52 +0100

    join ex 1

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -9,16 +9,16 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
 
-SELECT * FROM students WHERE YEAR(date_of_birth) < YEAR(DATE_SUB(CURRENT_DATE(), INTERVAL 30 YEAR));
+SELECT * FROM students WHERE YEAR(`date_of_birth`) < YEAR(DATE_SUB(CURRENT_DATE(), INTERVAL 30 YEAR));
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-SELECT * FROM `courses` WHERE `period` LIKE 'I semestre' AND `year` LIKE '1';
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = '1';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
 SELECT * FROM `exams` WHERE `date` = DATE('2020-06-20') AND `hour` >= TIME('14:00:00');
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
-SELECT * FROM `degrees` WHERE `level` LIKE '%magistrale%';
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
 SELECT COUNT(*) FROM `departments`;
@@ -40,4 +40,10 @@ SELECT COUNT(*) AS 'teachers_N' , `office_address` AS 'address' FROM `teachers` 
 SELECT `exam_id` AS 'exam', ROUND(AVG(`vote`)) AS 'avarege vote' FROM `exam_student` GROUP BY(`exam_id`);
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
-SELECT COUNT(`name`) AS 'corses', `department_id` AS 'departments' FROM `degrees` GROUP BY(`department_id`);
+SELECT COUNT(*) AS 'corses', `department_id` AS 'departments' FROM `degrees` GROUP BY(`department_id`);
+
+---------------------   Ex Join  --------------------------
+
+--1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` AS 'course' FROM `degrees` JOIN `students` ON `degrees`.`id` = `students`.`degree_id` WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
